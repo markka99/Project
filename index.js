209425bb1392271ccd713df28e2eadd7357c3e20
Alisher <alishernur7@gmail.com>
2022-01-24 19:32:16 +0300

    Film Game

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-alert(numberOfFilms);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
+
